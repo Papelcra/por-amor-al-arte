aaac4a42890e5c024844046628fedbd6a9a9a6c6
Papelcra <juliangarcia2038@gmail.com>
2026-03-31 21:15:50 -0500

    avance propio antes de sincronizar

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +11,8 @@ urlpatterns = [
     path('groups/', include('groups.urls')),
     path('catalog/', include('catalog.urls')),
     path('', include('core.urls')),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
