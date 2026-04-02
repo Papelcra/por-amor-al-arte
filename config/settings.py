3d63be56668685c9f1482cf23fc0928214492d12
Papelcra <juliangarcia2038@gmail.com>
2026-04-02 13:01:22 -0500

    Cambios en settings.py para bd de render

--- config/settings.py
+++ config/settings.py
@@ -101,11 +101,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # ====================== 🗄️ BASE DE DATOS ======================
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Si no encuentra la variable DATABASE_URL, usa SQLite por defecto (local)
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
+        conn_max_age=600
+    )
 }
 
 
